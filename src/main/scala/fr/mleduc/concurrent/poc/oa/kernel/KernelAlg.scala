46d73d33ceaa2e3cb75cdccbe0621f0ef1e032bb
Manuel Leduc <manuel.leduc@inria.fr>
2016-11-04 11:04:20 +0100

    cleanup actors part

--- src/main/scala/fr/mleduc/concurrent/poc/oa/kernel/KernelAlg.scala
+++ src/main/scala/fr/mleduc/concurrent/poc/oa/kernel/KernelAlg.scala
@@ -2,9 +2,6 @@ package fr.mleduc.concurrent.poc.oa.kernel
 
 import java.util.UUID
 
-import akka.actor.Actor.Receive
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-
 import scala.collection.mutable
 import scalax.collection.GraphEdge._
 import scalax.collection.GraphPredef._
@@ -40,8 +37,6 @@ trait KernelAlg {
 
 trait KernelAlgExec extends KernelAlg {
 
-
-
   override def debug(): Unit = println(opGraph)
 
   override type OperationId = UUID
@@ -54,7 +49,6 @@ trait KernelAlgExec extends KernelAlg {
 
   private val operationMap: mutable.Map[OperationId, PartialFunction[Any, Unit]] = mutable.Map.empty
 
-  // TODO : continuer a implem sur la base d'un graph. Lancer résolution du problème dans un thread à part ?
   private val opGraph = Graph[GraphOperations, DiEdge]()
 
   /**
@@ -65,7 +59,6 @@ trait KernelAlgExec extends KernelAlg {
     */
   override def createBroadcast(): OperationId = {
     val id = UUID.randomUUID()
-    //opGraph = opGraph + BroadcastNode(id)
     opGraph.add(BroadcastNode(id))
     id
   }
@@ -98,81 +91,6 @@ trait KernelAlgExec extends KernelAlg {
 
   override def startOperation(actorId: OperationId): Unit = {
     val pf = operationMap.getOrElse(actorId, PartialFunction.empty)
-    //opGraph.add(OperationalNode(actorId, pf))
     pf()
   }
-}
-
-
-trait KernelAlgExecActors /*extends KernelAlg*/ {
-  /*override */ type OperationId = ActorRef
-
-  private val system = ActorSystem("BroadActorSystem")
-
-  //private val uuidToActors: mutable.Map[ActorId, ActorRef] = mutable.Map.empty
-
-
-  sealed trait BroadcastActorActions
-
-  final case class RegisterMe(actorRef: ActorRef)
-
-  final case class BroadcastThis(message: Any)
-
-  class OperationalActor(receivez: Receive) extends Actor {
-    override def receive: Receive = {
-      receivez
-    }
-  }
-
-  class BroadcastActor() extends Actor {
-
-    private val registered: scala.collection.mutable.Set[ActorRef] = mutable.Set.empty
-
-
-    override def receive: Receive = {
-
-      case RegisterMe(actorRef) => {
-        registered.add(actorRef)
-      }
-      case BroadcastThis(message) =>
-        /*
-        Once every message is broadcasted, the registration list is cleared.
-         */
-        registered foreach (actorId => {
-          actorId ! message
-        })
-        registered.clear()
-    }
-  }
-
-  /**
-    * This type of actor broadcast a message to everybody subscribed to it.
-    * One a message is send to an subscribed actor, this actor is unsubscribed
-    *
-    * @return
-    */
-  /*override*/ def createBroadcast(): OperationId = {
-    /*val broadcastActor: ActorRef = this.system.actorOf(Props(new BroadcastActor()))
-    val id: UUID = UUID.randomUUID()
-    uuidToActors.put(id, broadcastActor)
-    id*/
-    this.system.actorOf(Props(new BroadcastActor()))
-  }
-
-  /*override*/ def broadcastMessage(actorId: OperationId, message: Any): Unit = {
-    actorId ! BroadcastThis(message)
-  }
-
-
-  /*override*/ def registerToBroadcast(actorId: OperationId, continuation: PartialFunction[Any, Unit]): Unit = {
-    val operationalActor = system.actorOf(Props(new OperationalActor(continuation)))
-    actorId ! RegisterMe(operationalActor)
-  }
-
-  /*override*/ def spawnOperation(operation: PartialFunction[Any, Unit]): OperationId = {
-
-    system.actorOf(Props(new OperationalActor(operation)))
-  }
-
-  /*override*/ def startOperation(actorId: ActorRef): Unit = actorId ! ()
 }
